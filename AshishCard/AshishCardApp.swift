eda6c878ff254244cadb1dd899375844b2ad684e
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-16 15:03:47 +0530

    Initial Commit

---
+++ AshishCard/AshishCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AshishCardApp.swift
+//  AshishCard
+//
+//  Created by Ashish Sharma on 12/16/2022.
+//
+
+import SwiftUI
+
+@main
+struct AshishCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
